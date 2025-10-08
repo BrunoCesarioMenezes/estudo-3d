8624d7687b432ba12f39ff54b8537f87803bae64
BrunoCesarioMenezes <brunoagjf@gmail.com>
2025-10-08 15:57:20 -0300

    commit para organizar os arquivos padroes do react

--- src/Box.jsx
+++ src/Box.jsx
@@ -3,7 +3,7 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import './css/globals.css'
 
-function Box(props) {
+export default function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
   // Hold state for hovered and clicked events
@@ -23,21 +23,5 @@ function Box(props) {
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
-  )
-}
-
-export default function App() {
-  return (
-    <div>
-      <h1 className="font-bold text-red-500">Testando</h1>
-      <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-        <OrbitControls />
-      </Canvas>
-    </div>
   )
 }
